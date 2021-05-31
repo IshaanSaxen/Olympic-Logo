b5da5b666e2c4b0c37b22b678e95387a2fc6c4c9
IshaanSaxen <78091439+IshaanSaxen@users.noreply.github.com>
2021-05-31 20:09:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidht = 10;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidht = 10;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidht = 10;
+ctx.arc(340, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidht = 10;
+ctx.arc(430, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth= 2;
+ctx.arc(300,250,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth= 2;
+ctx.arc(400,250,40,0,2 * Math.PI);
+ctx.stroke();
